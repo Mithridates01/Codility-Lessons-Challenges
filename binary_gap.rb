e2e30d95586246686e554060e0de522ffca266d6
Mithridates01 <jclucas21@gmail.com>
2017-03-23 17:00:06 -0500

    initial git commit from binary gap problem

---
+++ binary_gap.rb
@@ -0,0 +1,37 @@
+
+def decimal_to_binary(n)
+  integer = n
+  current_binary_number = []
+
+  while integer > 0 do
+    current_binary_number << integer % 2
+    integer = integer - ( ( integer / 2 ) + current_binary_number.last )
+  end
+
+  current_binary_number.reverse!.join("")
+end
+
+def collect_binary_gaps(binary_string)
+  gap_list = binary_string.scan(/1(0+)(?=1)/)
+  gap_list.sort_by! {|x, y| x.length <=> y.length}
+  gap_list[0].length
+end
+
+def solution(n)
+  return 0 if n == 0
+
+  binary_string = decimal_to_binary(n)
+  puts "binary #{binary_string}"
+
+  collect_binary_gaps(binary_string)
+end
+
+
+
+
+puts solution(0)
+puts solution(1)
+puts solution(2)
+puts solution(3)
+puts solution(10)
+puts solution(1041)
