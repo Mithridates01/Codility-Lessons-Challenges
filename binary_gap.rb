37c68537ae29fefb828cdd359923c3c2fcc2f7b4
Mithridates01 <jclucas21@gmail.com>
2017-03-23 17:04:53 -0500

    tested decimal_to_binary function; pass

--- binary_gap.rb
+++ binary_gap.rb
@@ -13,6 +13,7 @@ end
 
 def collect_binary_gaps(binary_string)
   gap_list = binary_string.scan(/1(0+)(?=1)/)
+  puts gap_list
   gap_list.sort_by! {|x, y| x.length <=> y.length}
   gap_list[0].length
 end
@@ -27,11 +28,11 @@ def solution(n)
 end
 
 
+puts decimal_to_binary(1402)
 
-
-puts solution(0)
-puts solution(1)
-puts solution(2)
-puts solution(3)
-puts solution(10)
-puts solution(1041)
+# puts solution(0)
+# puts solution(1)
+# puts solution(2)
+# puts solution(3)
+# puts solution(10)
+# puts solution(1041)
