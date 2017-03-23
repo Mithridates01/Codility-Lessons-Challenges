1f192ac14f3944c51ae6053c3937c5976591a8cd
Mithridates01 <jclucas21@gmail.com>
2017-03-23 17:41:36 -0500

    switch to pick last element in array within solution method

--- binary_gap.rb
+++ binary_gap.rb
@@ -12,7 +12,8 @@ def decimal_to_binary(n)
 end
 
 def collect_binary_gaps(binary_string)
-  binary_string.scan(/1(0+)(?=1)/).flatten
+  gaps = binary_string.scan(/1(0+)(?=1)/).flatten
+  p gaps
 end
 
 def sort_gaps(binary_gaps_array)
@@ -26,8 +27,6 @@ def sort_gaps(binary_gaps_array)
 end
 
 def solution(n)
-  return 0 if n == 0
-
   binary_string = decimal_to_binary(n)
   binary_gaps = collect_binary_gaps(binary_string)
   sort_gaps(binary_gaps).last
@@ -35,18 +34,18 @@ end
 
 
 # TESTING CODE
-binary_string = decimal_to_binary(14020)
-p binary_string
+# binary_string = decimal_to_binary(1041)
+# p binary_string
 
-gaps = collect_binary_gaps(binary_string)
-p gaps
+# gaps = collect_binary_gaps(binary_string)
+# p gaps
 
-sorted_gaps = sort_gaps([gaps])
-p sorted_gaps
+# sorted_gaps = sort_gaps([gaps])
+# p sorted_gaps
 
-# puts solution(0)
-# puts solution(1)
-# puts solution(2)
-# puts solution(3)
-# puts solution(10)
-# puts solution(1041)
+puts solution(0)
+puts solution(1)
+puts solution(2)
+puts solution(3)
+puts solution(10)
+puts solution(1041)
