ad236cefa9804ba152617f0cf800aa5a902df215
Josh Lucas <jclucas21@gmail.com>
2017-04-13 12:24:31 -0500

    setup basic test

--- arrays-lesson-2.js
+++ arrays-lesson-2.js
@@ -1,11 +1,17 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
-
+//test array
+var testArray = [6,6,3,4,4,3,7,77,7,77,100,10000,100];
 
 function solution(array) {
+
 
 }
 
 
+var result = solution(testArray);
 
+if (result === 10000){
+  console.log("true");
+}
