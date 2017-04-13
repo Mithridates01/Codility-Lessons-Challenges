9faa5729ae74eae7ea871a2a652e00f2ae5daaa5
Josh Lucas <jclucas21@gmail.com>
2017-04-13 12:30:03 -0500

    initial sudo code

--- arrays-lesson-2.js
+++ arrays-lesson-2.js
@@ -5,7 +5,10 @@
 var testArray = [6,6,3,4,4,3,7,77,7,77,100,10000,100];
 
 function solution(array) {
-
+  //have two variables, one matched and one unmatched
+  //unmatched integer variable
+  //iterate over array of integers
+  //
 
 }
 
@@ -15,3 +18,12 @@ var result = solution(testArray);
 if (result === 10000){
   console.log("true");
 }
+
+
+// input:
+// array of matched pairs of
+// integers with one unmatched integer
+
+// Output:
+// return a single unmatched integer
+
