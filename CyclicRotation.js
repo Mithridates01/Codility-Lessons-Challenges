c0bf9447802a980f356b991ce57ad0dfafdb676b
Josh Lucas <jclucas21@gmail.com>
2017-04-17 10:31:22 -0500

    initial setup/commit

---
+++ CyclicRotation.js
@@ -0,0 +1,6 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(array, rotations) {
+    // write your code in JavaScript (Node.js 6.4.0)
+}
