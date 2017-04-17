1bf826b1faf5e538bcad67e332878585c73cf7e8
Josh Lucas <jclucas21@gmail.com>
2017-04-17 11:31:42 -0500

    working test cases

--- CyclicRotation.js
+++ CyclicRotation.js
@@ -10,26 +10,36 @@
 // return new array
 
 function solution(array, rotations) {
+  console.log("initial array: ", array)
   var rotateNum;
   var currentRotationElements;
 
   while (rotations > 0){
-    console.log(rotations)
+    console.log("rotations: ", rotations);
     //prevent error if rotations are longer than the array
     if (rotations > array.length){
       rotateNum = rotations - array.length;
       rotations -= rotateNum;
+      console.log("rotations: ", rotations);
+      console.log("rotateNum:", rotateNum);
     } else {
       rotateNum = rotations;
       rotations -= rotateNum;
+      console.log("rotations: ", rotations);
+      console.log("rotateNum:", rotateNum);
     }
-
+    console.log("before: ", array)
     currentRotationElements = array.splice(-rotateNum);
     array = currentRotationElements.concat(array);
+    console.log("after: ", array);
   }
   return array;
 }
 
+// testing
 
-
-
+console.log("return value: ", solution( [1,2,3,4,5,6,6,7,8], 5 ) );
+// console.log( solution( [1,2], 3 ) );
+// console.log( solution( [1,2,3,4,5,6,6,7,8], 20 ) );
+// console.log( solution( [1,2,3,4,5,6,6,7,8], 3 ) );
+// console.log( solution( [1,2,3], 3 ) );
