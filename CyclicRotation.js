00ab63079003679f5a17249ae09bfaeabee7ee40
Josh Lucas <jclucas21@gmail.com>
2017-04-17 14:01:12 -0500

    finish

--- CyclicRotation.js
+++ CyclicRotation.js
@@ -9,6 +9,8 @@
 // repeat until rotations == 0
 // return new array
 
+
+
 function solution(array, rotations) {
   var rotateNum;
   var currentRotationElements;
@@ -31,10 +33,59 @@ function solution(array, rotations) {
 
 // testing
 
-console.log("return value: ", solution( [1,2,3,4,5,6,6,7,8], 5 ) );
-console.log( solution( [1,2], 3 ) );
-console.log( solution( [1,2,3,4,5,6,6,7,8], 20 ) );
-console.log( solution( [1,2,3,4,5,6,6,7,8], 3 ) );
-console.log( solution( [1,2,3], 3 ) );
-console.log( solution( [],5) )
-console.log( solution( [1],5) )
+var is_same = (array1.length == array2.length) && array1.every(function(element, index) {
+    return element === array2[index];
+});
+
+  console.log( solution( [1,2,3,4,5,6,6,7,8], 5 ) );
+  debugger
+if ( solution( [1,2,3,4,5,6,6,7,8], 5 ) == [ 5, 6, 6, 7, 8, 1, 2, 3, 4 ] ) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+  console.log( solution( [1,2], 3 ) );
+if ( solution( [1,2], 3 ) == [ 2, 1 ] ) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+  console.log( solution( [1,2,3,4,5,6,6,7,8], 20 ) );
+if ( solution( [1,2,3,4,5,6,6,7,8], 20 ) == [ 1, 2, 3, 4, 5, 6, 6, 7, 8 ] ) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+  console.log( solution( [1,2,3,4,5,6,6,7,8], 3 ) );
+if ( solution( [1,2,3,4,5,6,6,7,8], 3 ) == [ 6, 7, 8, 1, 2, 3, 4, 5, 6 ] ) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+  console.log( solution( [1,2,3], 3 ) );
+if ( solution( [1,2,3], 3 ) == [ 1, 2, 3 ] ) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+  console.log( solution( [],5) );
+if ( solution( [],5) == [] ) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+if ( solution( [1],5) === [ 1 ] ) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+// Initial scoring was 87% correctness: fail edge case for
+//  input: ([1, 1, 2, 3, 5], 42)
+//  (got [1, 1, 2, 3, 5] expected [3, 5, 1, 1, 2])
